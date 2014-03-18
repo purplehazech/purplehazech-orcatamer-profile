ea0268c4c00b09cc7c49ce7d2cb7fb9a31d9113d
Lucas Bickel <hairmare@purplehaze.ch>
2014-03-19 00:06:01 +0100

    document why this is here in its abandond state

--- manifests/pumpio.pp
+++ manifests/pumpio.pp
@@ -1,5 +1,21 @@
 # # Class: profile::pumpio
 #
+# Install a pump.io server.
+#
+# This module is very incomplete due to the various reasons.
+# * pump.io is hard to run on shared infrastructure
+# * running pump.io behind a proxy does not seem sensible
+#
+# I'm leaving this here for now. As pump.io matures I might
+# use it again to build a pump.io server.
+#
+# Here are more things that I want pump.io to do before I
+# consider it again.
+# * easy way to swap out ``/`` page with some static content
+#   of my own liking.
+# * way to either run behind a proxy or maybe even act as a
+#   proxy so I have a way to replicate the various parts my
+#   legacy apache setup has accumulated.
 #
 class profile::pumpio {
   file { [
