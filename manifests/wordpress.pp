82dd76a8b2df771314586ef0e95f7c1316eb9246
Lucas Bickel <hairmare@purplehaze.ch>
2014-03-21 13:27:36 +0100

    basic fpm/mysql based wordpress install

--- manifests/wordpress.pp
+++ manifests/wordpress.pp
@@ -2,4 +2,27 @@
 #
 #
 class profile::wordpress {
+  package_use { 'app-admin/eselect-php':
+    use => [
+      'fpm',
+    ]
+  } ->
+  package_use { 'dev-lang/php':
+    use => [
+      'gd',
+      'truetype',
+      'fpm',
+      'mysql',
+    ]
+  } ->
+  portage::package { 'www-apps/wordpress':
+    ensure   => installed,
+    alias    => 'wordpress',
+    use      => [
+      'vhosts'
+    ],
+    keywords => [
+      '~amd64',
+    ],
+  }
 }
